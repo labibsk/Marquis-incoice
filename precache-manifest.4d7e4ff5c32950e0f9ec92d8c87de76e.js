5b15a61adc1f42439fea20b752902093f5d6f472
Ismail <ismail.sheikh.5432@gmail.com>
2025-04-23 16:05:26 +0530

    Updates

---
+++ precache-manifest.4d7e4ff5c32950e0f9ec92d8c87de76e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eee579f9acb8b472aac4df3c7087386",
+    "url": "/Marquis-incoice/index.html"
+  },
+  {
+    "revision": "db17834bc4f706b7b06b",
+    "url": "/Marquis-incoice/static/css/main.abb48311.chunk.css"
+  },
+  {
+    "revision": "d96692df0250bc43e0aa",
+    "url": "/Marquis-incoice/static/js/2.1dfbe3a3.chunk.js"
+  },
+  {
+    "revision": "88011752e4ff9ce31dfb431496cb8e29",
+    "url": "/Marquis-incoice/static/js/2.1dfbe3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db17834bc4f706b7b06b",
+    "url": "/Marquis-incoice/static/js/main.43226ce0.chunk.js"
+  },
+  {
+    "revision": "ec66588175ba7cfca3c4",
+    "url": "/Marquis-incoice/static/js/runtime-main.f9cf8fef.js"
+  },
+  {
+    "revision": "09332992bfbd121cd56290d75faa8193",
+    "url": "/Marquis-incoice/static/media/marquis_logo_short.09332992.png"
+  },
+  {
+    "revision": "2746c9cded29be312cc960d5427a3c53",
+    "url": "/Marquis-incoice/static/media/recta-smallcaps.2746c9cd.otf"
+  }
+]);
